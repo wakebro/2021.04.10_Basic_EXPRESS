4303f4f44ca7a137426478904214ebc7d58dae64
wake <wakebro119@naver.com>
2021-05-11 17:03:29 +0900

    리팩토링

--- main.js
+++ main.js
@@ -34,69 +34,7 @@ app.get("/flash-display", function (req, res) {
   res.render("index", { messages: req.flash("info") });
 });
 
-// passport에 사용
-const authData = {
-  email: "test@test.com",
-  password: "111111",
-  nickname: "tester",
-};
-
-// session 이후에 동작하므로 session 다음에 선언
-var passport = require("passport"),
-  LocalStrategy = require("passport-local").Strategy;
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-// 로그인 시 성공하여 한번 호출되어 사용자의 식별자를 로컬스토어에 저장
-passport.serializeUser(function (user, done) {
-  console.log("serializeUser", user);
-  done(null, user.email);
-});
-
-// 위 serializeUser에 저장된 데이터를 기준으로 필요한 데이터를 조회
-passport.deserializeUser(function (id, done) {
-  console.log("deserializeUser", id);
-  done(null, authData);
-});
-
-// 로그인 프로세스
-passport.use(
-  new LocalStrategy(
-    {
-      usernameField: "email",
-      passwordField: "pwd",
-    },
-    function (username, password, done) {
-      console.log("LocalStrategy", username, password);
-      if (username === authData.email) {
-        console.log(1);
-        if (password === authData.password) {
-          console.log(2);
-          return done(null, authData);
-        } else {
-          console.log(3);
-          return done(null, false, { message: "Incorrect password." });
-        }
-      } else {
-        console.log(4);
-        return done(null, false, { message: "Incorrect username." });
-      }
-    }
-  )
-);
-
-// 로그인 passport로 변경
-// 로그인 성공 여부에 따라 이동
-app.post(
-  "/auth/login_process",
-  passport.authenticate("local", {
-    successRedirect: "/",
-    failureRedirect: "/auth/login",
-    failureFlash: true,
-    successFlash: true,
-  })
-);
+let passport = require("./lib/passport")(app);
 
 // app.get("*",()=>{})_get방식으로 들어오는 요청에 대해서만 처리
 app.get("*", (request, response, next) => {
@@ -111,7 +49,7 @@ app.use(helmet());
 // 라우터 모듈 불러오기
 const indexRouter = require("./routes/index");
 const topicRouter = require("./routes/topic");
-const authRouter = require("./routes/auth");
+const authRouter = require("./routes/auth")(passport);
 
 app.use("/", indexRouter);
 app.use("/topic", topicRouter);
--- routes/auth.js
+++ routes/auth.js
@@ -6,66 +6,50 @@ const sanitizeHtml = require("sanitize-html");
 const template = require("../lib/template.js");
 const session = require("express-session");
 
-// passpoart로 변경
-/*
-const authData = {
-  email: "test@test.com",
-  password: "111111",
-  nickname: "tester",
-};
-*/
+module.exports = function (passport) {
+  router.get("/login", (request, response) => {
+    var fmsg = request.flash();
+    var feedbak = "";
+    if (fmsg.error) {
+      feedbak = fmsg.error[0];
+    }
+    var title = "WEB - login";
+    var list = template.list(request.list);
+    var html = template.HTML(
+      title,
+      list,
+      `
+        <div style="color:blue;">${feedbak}</div>
+        <form action="/auth/login_process" method="post">
+          <p><input type="email" name="email" placeholder="email"></p>
+          <p><input type="password" name="pwd" placeholder="password"></p>
+          <p><input type="submit" value='login'></p>
+        </form>
+      `,
+      ""
+    );
+    response.send(html);
+  });
 
-router.get("/login", (request, response) => {
-  var fmsg = request.flash();
-  var feedbak = "";
-  if (fmsg.error) {
-    feedbak = fmsg.error[0];
-  }
-  var title = "WEB - login";
-  var list = template.list(request.list);
-  var html = template.HTML(
-    title,
-    list,
-    `
-      <div style="color:blue;">${feedbak}</div>
-      <form action="/auth/login_process" method="post">
-        <p><input type="email" name="email" placeholder="email"></p>
-        <p><input type="password" name="pwd" placeholder="password"></p>
-        <p><input type="submit" value='login'></p>
-      </form>
-    `,
-    ""
+  // 로그인 성공 여부에 따라 이동
+  router.post(
+    "/login_process",
+    passport.authenticate("local", {
+      successRedirect: "/",
+      failureRedirect: "/auth/login",
+      failureFlash: true,
+      successFlash: true,
+    })
   );
-  response.send(html);
-});
 
-// login_process를 passport로 변경
-/*
-router.post("/login_process", (request, response) => {
-  var post = request.body;
-  var email = post.email;
-  var password = post.pwd;
-  if (email === authData.email && password === authData.password) {
-    request.session.is_logined = true;
-    request.session.nickname = authData.nickname;
-    // 세션이 저장된 후 리다이렉션 실행
+  router.get("/logout", (request, response) => {
+    request.logOut();
+    // request.session.destroy((err) => {
+    //   response.redirect("/");
+    // });
     request.session.save(() => {
       response.redirect("/");
     });
-  } else {
-    response.end("Who?");
-  }
-  //response.end();
-});
-*/
-
-router.get("/logout", (request, response) => {
-  request.logOut();
-  // request.session.destroy((err) => {
-  //   response.redirect("/");
-  // });
-  request.session.save(() => {
-    response.redirect("/");
   });
-});
-module.exports = router;
+  return router;
+};
